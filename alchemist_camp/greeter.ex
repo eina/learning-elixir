6844790efba2a9631d6e74d66f83b3bd16195f40
Eina Onting <einers@outlook.com>
2020-10-24 14:57:09 -0700

    refactor greeter based on alchemist camp solution

--- alchemist_camp/greeter.ex
+++ alchemist_camp/greeter.ex
@@ -2,12 +2,17 @@
 # make a program that greets a user by name, if they enter your name, it should say something special.
 
 defmodule Greeter do
-  answer = IO.gets("Hi there! What's your name? \n")
-  trimmed = String.trim(answer)
-  normalized = String.downcase(trimmed)
+  # module attribute
+  @author "Eina"
 
-  case(normalized) do
-    "eina" -> IO.puts("No, you're name isn't #{trimmed}! I'm the only allowed to have that name!")
-    _ -> IO.puts("Nice to meet you, #{trimmed}!")
-  end  
+  def start do
+    # assign to answer, then trim and reassign that to answer
+    answer = IO.gets("Hi there! What's your name? \n") |> String.trim()    
+
+    if (String.downcase(answer)) == String.downcase(@author) do    
+      "No, your name isn't #{@author}! I'm the only allowed to have that name!"
+    else 
+      "Nice to meet you, #{answer}!"
+    end  
+  end
 end
